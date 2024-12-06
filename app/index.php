77a9df2cfe34eca91108f60bec9fda48cdd8ff7c
ezequielmartinb10@gmail.com <ezequielmartinb10@gmail.com>
2024-12-06 20:17:07 -0300

    TP FINALIZADO

--- app/index.php
+++ app/index.php
@@ -2,7 +2,7 @@
 // Error Handling
 error_reporting(-1);
 ini_set('display_errors', 1);
-
+// php -S localhost:666 -t app
 use Dotenv\Validator;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
